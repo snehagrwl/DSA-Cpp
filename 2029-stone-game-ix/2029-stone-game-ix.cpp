d8a1e2736b85fd653b92d8fee81d96ca2e5996cd
Sneha Agrawal <agrawalsneha1305@gmail.com>
2026-08-16 12:13:29 +0530

    Time: 4 ms (62.44%), Space: 131.2 MB (69.29%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int c0 = 0, c1 = 0, c2 = 0;
+        
+        for (int x : stones) {
+            if (x % 3 == 0) c0++;
+            else if (x % 3 == 1) c1++;
+            else c2++;
+        }
+        
+        // Even number of 0s: Alice wins if both 1s and 2s exist
+        if (c0 % 2 == 0) {
+            return c1 > 0 && c2 > 0;
+        }
+        
+        // Odd number of 0s: Alice wins if the difference is strictly greater than 2
+        return abs(c1 - c2) > 2;
+    }
+};
