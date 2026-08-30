3486b041774248bc8af77baca4848ec295fe88ac
Sneha Agrawal <agrawalsneha1305@gmail.com>
2026-08-31 00:55:59 +0530

    Time: 10 ms (6.69%), Space: 92.1 MB (22.14%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,3 +1,10 @@
+
+static const int fast_io = []() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    return 0;
+}();
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
